03b15d952fa72e814571d15a7a7d8f12afa28537
Kathode <work.qiyan@gmail.com>
2024-09-14 13:31:01 +1000

    Add tile variants export

---
+++ src/tiles.js
@@ -0,0 +1,8 @@
+import Tile1 from "./assets/tile-1.jpeg";
+import Tile2 from "./assets/tile-2.jpeg";
+import Tile3 from "./assets/tile-3.jpeg";
+import Tile4 from "./assets/tile-4.jpeg";
+import Tile5 from "./assets/tile-5.jpeg";
+import Tile6 from "./assets/tile-6.jpeg";
+
+export { Tile1, Tile2, Tile3, Tile4, Tile5, Tile6 };
