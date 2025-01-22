f555a64cd141925a9c725e8f6882072ae2b8dcd0
Kathode <work.qiyan@gmail.com>
2025-01-22 12:38:53 +1100

    Import svg via css instead of js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import "./styles.css";
-import svg from "./assets/wave-haikei.svg";
 
 const content = document.querySelector("#content");
 
 (function () {
   const restaurantWall = document.createElement("div");
-  const backgroundSvg = document.createElement("img");
 
   restaurantWall.classList.add("restaurant-wall");
-  backgroundSvg.src = svg;
-  restaurantWall.appendChild(backgroundSvg);
+
   content.appendChild(restaurantWall);
 })();
