71915acc417a9566ec9470953b3dc6866728b8d1
Kathode <work.qiyan@gmail.com>
2025-01-23 21:48:33 +1100

    Add javascript to layout the elements and effect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import "./styles.css";
+import banhMi from "./assets/banh-mi/Bánh-mì.png";
 
 const content = document.querySelector("#content");
 
 (function () {
-  const homeLink = document.createElement("a");
-  const menuLink = document.createElement("a");
-  const contactLink = document.createElement("a");
   const restaurantWall = document.createElement("div");
   const restaurantWallBlobPink = document.createElement("div");
   const restaurantWallBlobGreen = document.createElement("div");
@@ -13,14 +11,63 @@ const content = document.querySelector("#content");
   restaurantWall.classList.add("restaurant-wall", "wall-wave");
   restaurantWallBlobPink.classList.add("restaurant-wall", "wall-blob-pink");
   restaurantWallBlobGreen.classList.add("restaurant-wall", "wall-blob-green");
-  homeLink.textContent = "Home";
-  menuLink.textContent = "Menu";
-  contactLink.textContent = "Contact";
 
   content.appendChild(restaurantWall);
   content.appendChild(restaurantWallBlobPink);
   content.appendChild(restaurantWallBlobGreen);
-  content.appendChild(homeLink);
-  content.appendChild(menuLink);
-  content.appendChild(contactLink);
+})();
+
+(function () {
+  const homeLink = document.createElement("a");
+  const menuLink = document.createElement("a");
+  const contactLink = document.createElement("a");
+  const title = document.createElement("h1");
+  const banhMiImg = document.createElement("img");
+  const plateContainer = document.createElement("div");
+  const container = document.createElement("div");
+  const square = document.createElement("div");
+
+  const menuPromptWrapper = document.createElement("div");
+  const arrowImg = document.createElement("div");
+  const prompt = document.createElement("h2");
+
+  homeLink.textContent = "Home";
+  menuLink.textContent = "Menu";
+  contactLink.textContent = "Contact";
+  title.textContent = "Bánh Mì Bliss";
+  prompt.textContent = "Eat me";
+  banhMiImg.src = banhMi;
+
+  plateContainer.addEventListener("mouseover", () => {
+    plateContainer.style.scale = 1.1;
+    arrowImg.style.transform = "rotate(-50deg)";
+    menuPromptWrapper.style.opacity = 1;
+  });
+
+  plateContainer.addEventListener("mouseleave", () => {
+    plateContainer.style.scale = 1;
+    arrowImg.style.transform = "rotate(-60deg)";
+    menuPromptWrapper.style.opacity = 0;
+  });
+
+  container.classList.add("container");
+  square.classList.add("square");
+  title.classList.add("title");
+  banhMiImg.classList.add("banh-mi-img");
+  plateContainer.classList.add("plate-container");
+
+  menuPromptWrapper.classList.add("menu-prompt-wrapper");
+  prompt.classList.add("prompt");
+  arrowImg.classList.add("arrow-image");
+
+  menuPromptWrapper.appendChild(arrowImg);
+  menuPromptWrapper.appendChild(prompt);
+
+  plateContainer.appendChild(banhMiImg);
+  square.appendChild(title);
+  square.appendChild(plateContainer);
+  square.appendChild(menuPromptWrapper);
+
+  container.appendChild(square);
+  content.appendChild(container);
 })();
