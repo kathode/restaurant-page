596c57e05e6e8b6fd57e70285dd4d153dabf4573
Kathode <work.qiyan@gmail.com>
2025-02-05 15:43:09 +1100

    Add banh mi assets into js file

---
+++ src/banhmi.js
@@ -0,0 +1,10 @@
+import BanhMiChaLua from "./assets/banh-mi/Bánh-mì-Chả-Lụa.jpeg";
+import BanhMiChay from "./assets/banh-mi/Bánh-Mì-Chay.jpeg";
+import BanhMiDacBiet from "./assets/banh-mi/Bánh-mì-Đặc-Biệt.jpeg";
+import BanhMiGaXe from "./assets/banh-mi/Bánh-mì-Gà-Xé.jpeg";
+import BanhMiNemNuong from "./assets/banh-mi/Bánh-mì-Nem-Nướng.jpeg";
+import BanhMiThitNguoi from "./assets/banh-mi/Bánh-mì-Thịt-Nguội.jpeg";
+import BanhMiThitNuong from "./assets/banh-mi/Bánh-mì-Thịt-Nướng.jpeg";
+import BanhMiXaXiu from "./assets/banh-mi/Bánh-mì-Xá-Xíu.jpeg";
+
+export { BanhMiChaLua, BanhMiChay, BanhMiDacBiet, BanhMiGaXe, BanhMiNemNuong, BanhMiThitNguoi, BanhMiThitNuong, BanhMiXaXiu };
