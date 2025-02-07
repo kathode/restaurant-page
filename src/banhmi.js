b3327ba0d0a6982181510d893c4056dbd4d8820f
Kathode <work.qiyan@gmail.com>
2025-02-07 12:55:19 +1100

    Add banh mi menu data

--- src/banhmi.js
+++ src/banhmi.js
@@ -7,4 +7,13 @@ import BanhMiThitNguoi from "./assets/banh-mi/Bánh-mì-Thịt-Nguội.jpeg";
 import BanhMiThitNuong from "./assets/banh-mi/Bánh-mì-Thịt-Nướng.jpeg";
 import BanhMiXaXiu from "./assets/banh-mi/Bánh-mì-Xá-Xíu.jpeg";
 
-export { BanhMiChaLua, BanhMiChay, BanhMiDacBiet, BanhMiGaXe, BanhMiNemNuong, BanhMiThitNguoi, BanhMiThitNuong, BanhMiXaXiu };
+export const BanhMiSources = [
+  { src: BanhMiChaLua, westernName: "Pork Loaf", vietName: "Bánh mì Chả Lụa", price: 6.5 },
+  { src: BanhMiChay, westernName: "Vegetarian", vietName: "Bánh mì Chay", price: 6.0 },
+  { src: BanhMiDacBiet, westernName: "Special", vietName: "Bánh mì Đặc Biệt", price: 7.5 },
+  { src: BanhMiGaXe, westernName: "Shredded Chicken", vietName: "Bánh mì Gà Xé", price: 6.8 },
+  { src: BanhMiNemNuong, westernName: "Grilled Pork Patty", vietName: "Bánh mì Nem Nướng", price: 7.0 },
+  { src: BanhMiThitNguoi, westernName: "Cold Cuts", vietName: "Bánh mì Thịt Nguội", price: 7.2 },
+  { src: BanhMiThitNuong, westernName: "Grilled Pork", vietName: "Bánh mì Thịt Nướng", price: 7.0 },
+  { src: BanhMiXaXiu, westernName: "BBQ Pork", vietName: "Bánh mì Xá Xíu", price: 7.3 },
+];
