e0768856a25f61af634fba4fa521775afe54c123
Kathode <work.qiyan@gmail.com>
2025-02-07 13:18:38 +1100

    Add functionality and create elements to display and hide the menu list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import banhMi from "./assets/banh-mi/Bánh-mì.png";
+import { BanhMiSources } from "./banhmi";
 
 const content = document.querySelector("#content");
 
@@ -18,11 +19,48 @@ const content = document.querySelector("#content");
 })();
 
 (function () {
-  const homeLink = document.createElement("a");
-  const menuLink = document.createElement("a");
-  const contactLink = document.createElement("a");
   const title = document.createElement("h1");
   const banhMiImg = document.createElement("img");
+
+  const banhMiElements = BanhMiSources.map((image) => {
+    const img = document.createElement("img");
+    const imgWrapper = document.createElement("div");
+    const card = document.createElement("div");
+    const textContainer = document.createElement("div");
+
+    const westernName = document.createElement("div");
+    const vietName = document.createElement("div");
+    const price = document.createElement("div");
+
+    img.src = image.src;
+    westernName.textContent = image.westernName;
+    vietName.textContent = image.vietName;
+    price.textContent = `$${image.price}`;
+    img.classList.add("banh-mi-image");
+    imgWrapper.classList.add("banh-mi-image-wrapper");
+    textContainer.classList.add("text-container");
+    price.classList.add("banh-mi-price");
+    westernName.classList.add("banh-mi-western-name");
+    vietName.classList.add("banh-mi-viet-name");
+    card.classList.add("card");
+
+    imgWrapper.append(img);
+    textContainer.append(westernName, vietName, price);
+    card.append(imgWrapper, textContainer);
+
+    card.addEventListener("mouseenter", () => {
+      img.style.filter = "brightness(1.1)";
+      img.style.transform = "scale(1.05) translate(-5%, -5%)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+      img.style.filter = "brightness(0.8)";
+      img.style.transform = "scale(0.9) translate(-5%, -5%)";
+    });
+
+    return card;
+  });
+
   const plateContainer = document.createElement("div");
   const plateOutline = document.createElement("div");
   const container = document.createElement("div");
@@ -33,15 +71,15 @@ const content = document.querySelector("#content");
   const contactPhoneImage = document.createElement("div");
 
   const menuPromptWrapper = document.createElement("div");
+  const menuList = document.createElement("div");
+  const menuListExitButton = document.createElement("button");
   const arrowImg = document.createElement("div");
   const prompt = document.createElement("h2");
 
-  homeLink.textContent = "Home";
-  menuLink.textContent = "Menu";
-  contactLink.textContent = "Contact";
   title.textContent = "Bánh Mì Bliss";
   prompt.textContent = "Eat me";
   banhMiImg.src = banhMi;
+  menuListExitButton.textContent = "x";
 
   plateOutline.addEventListener("mouseover", () => {
     plateOutline.style.scale = 1.1;
@@ -55,6 +93,14 @@ const content = document.querySelector("#content");
     menuPromptWrapper.style.opacity = 0;
   });
 
+  plateOutline.addEventListener("click", () => {
+    menuList.style.display = "grid";
+  });
+
+  menuListExitButton.addEventListener("click", () => {
+    menuList.style.display = "none";
+  });
+
   container.classList.add("container");
   square.classList.add("square");
   title.classList.add("title");
@@ -62,6 +108,8 @@ const content = document.querySelector("#content");
   plateContainer.classList.add("plate-container");
   plateOutline.classList.add("plate-outline");
   contactContainer.classList.add("contact-container");
+  menuList.classList.add("menu-list");
+  menuListExitButton.classList.add("menu-list-exit-button");
 
   contactOrange.classList.add("contact-orange");
   contactPhoneImage.classList.add("contact-phone-image");
@@ -85,4 +133,7 @@ const content = document.querySelector("#content");
 
   container.appendChild(square);
   content.appendChild(container);
+
+  menuList.append(...banhMiElements, menuListExitButton);
+  content.appendChild(menuList);
 })();
